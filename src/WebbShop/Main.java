16e9b08c1150522bed8aefbdeced04aa5eccfa6a
KareemKDX <drammeh@msn.com>
2024-02-12 11:55:54 +0100

    some new stuff

--- src/WebbShop/Main.java
+++ src/WebbShop/Main.java
@@ -11,10 +11,13 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        try {
 
+        try {
+            UserPrompt userPrompt = new UserPrompt();
 
             Repository repository = new Repository();
+
+
 
             Scanner scan = new Scanner(System.in);
 
@@ -28,66 +31,80 @@ public class Main {
 
             if (repository.validateUser(userName, passWord)) {
 
-                Repository.MenuTextUser();
 
-                int choice = scan.nextInt();
+               while(true) {
+                   boolean continueLoop = false;
 
-                List<CategoryTable> categoryList = repository.getCategories();
-                List<ProductTable> productList = repository.getProduct();
-
-                switch (choice) {
-
-                    case 1:
-                        case 2:
-                            case 3:
-                                case 4:
+                   userPrompt.UserFirstMenu();
 
 
-                    case 5:
-                        System.out.println("Svara nedan för att välja produkt:");
+                   int choice = scan.nextInt();
 
-                        productList.stream()
-                             .filter(p -> p.getCategoryId() == choice)
-                              .forEach(p -> System.out.println(p.printInfo()));
+                   List<CategoryTable> categoryList = repository.getCategories();
+                   List<ProductTable> productList = repository.getProduct();
 
+                   switch (choice) {
 
-                        //Märke Input
-                        System.out.print("Märke: ");
-                        String brandChoice = scan.next();
-                        //Storlek input
-                        System.out.print("Storlek: ");
-                        int sizeChoice = scan.nextInt();
-
-                       //Hämtar produktID baserat på svar och sätter i AddToCart
-                        int productID = repository.getProductID(sizeChoice, brandChoice);
-                        System.out.println("Product ID: " + productID);
-                        int orderID = 0;
-                       repository.addToCart(userID, orderID, productID);
-
-                        System.out.println("Kvitto");
-
-                        Stream.of(productID)
-                                .forEach(System.out::println);
+                       case 1:
+                       case 2:
+                       case 3:
+                       case 4:
 
 
+                       case 5:
+                           System.out.println("Svara nedan för att välja produkt:");
+
+                           productList.stream()
+                                   .filter(p -> p.getCategoryId() == choice)
+                                   .forEach(p -> System.out.println(p.printInfo()));
 
 
+                           //Märke Input
+                           System.out.print("Märke: ");
+                           String brandChoice = scan.next();
+                           //Storlek input
+                           System.out.print("Storlek: ");
+                           int sizeChoice = scan.nextInt();
+
+                           //Hämtar produktID baserat på svar och sätter i AddToCart
+                           int productID = repository.getProductID(sizeChoice, brandChoice);
+                           //System.out.println("Product ID: " + productID);
+                           int orderID = 0;
+                           repository.addToCart(userID, orderID, productID);
+
+                           System.out.println("Vill du beställa mer? JA/NEJ");
+                           String orderMoreInput = scan.next();
+
+                           if (orderMoreInput.equalsIgnoreCase("JA")) {
+                               userPrompt.UserFirstMenu();
+
+                           } else if (orderMoreInput.equalsIgnoreCase("nej")) {
+                               System.out.println("Kvitto");
+                               String receipt = productList.stream()
+                                       .filter(p -> p.getId() == productID)
+                                       .map(p -> String.format("Märke: %s, Storlek: %d, Pris: %d kr", p.getBrand(), p.getSize(), p.getPrice()))
+                                       .collect(Collectors.joining("\n"));
+                               System.out.println(receipt);
+
+                           }
 
 
-                        break;
-                    case 6:
-                        System.out.println("Avslutar");
-                        break;
+                           break;
+                       case 6:
+                           System.out.println("Avslutar");
+                           break;
 
-                    default:
-                        System.out.println("ogiltigt svar");
-                }
-               // int choiche2 = scan.nextInt();
-              //  System.out.println("du valde test " + choiche2);
+                       case 7:
+                           //Här vill vi få User-information namn, tfn-nummer, beställning
+
+                       default:
+                           System.out.println("ogiltigt svar");
+                   }
+                   // int choiche2 = scan.nextInt();
+                   //  System.out.println("du valde test " + choiche2);
 
 
-
-
+               }
 
             } else {
                 System.out.println("inloggning funkaar ej");
---
+++ src/WebbShop/UserPrompt.java
@@ -0,0 +1,29 @@
+package WebbShop;
+
+public class UserPrompt {
+    public UserPrompt() {}
+
+
+
+    public void UserFirstMenu() {
+        System.out.println("Inloggning gick igenom!");
+        System.out.println();
+        System.out.println();
+        System.out.println("Välj Kategori");
+        System.out.println("1. Boots");
+        System.out.println("2. Finskor");
+        System.out.println("3. Outdoor");
+        System.out.println("4. Sneakers");
+        System.out.println("5. Träning");
+        System.out.println("6. Avsluta");
+        System.out.println("7. Personlig Information");
+    }
+
+    public void orderMoreInput() {
+
+
+    }
+
+
+
+}
